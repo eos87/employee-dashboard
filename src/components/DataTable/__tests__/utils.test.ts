c3febff737ce668c772f1646aaffc6e1052e78f9
Helmy Giacoman <helmygb@gmail.com>
2023-05-20 10:49:31 +0200

    Add tests for sort function

---
+++ src/components/DataTable/__tests__/utils.test.ts
@@ -0,0 +1,45 @@
+import { sortData } from "../utils";
+
+describe("sortData", () => {
+    it("should sort data in ascending order for column with numbers", () => {
+        const data = [
+            { name: "Mario", age: 35 },
+            { name: "Andrea", age: 15 },
+            { name: "Sofia", age: 40 },
+        ];
+        const expected = [
+            { name: "Andrea", age: 15 },
+            { name: "Mario", age: 35 },
+            { name: "Sofia", age: 40 },
+        ];
+        expect(sortData(data, "asc", "age")).toEqual(expected);
+    });
+
+    it("should sort data in descending order for column with numbers", () => {
+        const data = [
+            { name: "Mario", age: 35 },
+            { name: "Andrea", age: 15 },
+            { name: "Sofia", age: 40 },
+        ];
+        const expected = [
+            { name: "Sofia", age: 40 },
+            { name: "Mario", age: 35 },
+            { name: "Andrea", age: 15 },
+        ];
+        expect(sortData(data, "desc", "age")).toEqual(expected);
+    });
+
+    it("should sort data in ascending order for a column with strings", () => {
+        const data = [
+            { name: "Mario", age: 35 },
+            { name: "Andrea", age: 15 },
+            { name: "Sofia", age: 40 },
+        ];
+        const expected = [
+            { name: "Andrea", age: 15 },
+            { name: "Mario", age: 35 },
+            { name: "Sofia", age: 40 },
+        ];
+        expect(sortData(data, "asc", "name")).toEqual(expected);
+    });
+});
--- src/components/DataTable/utils.ts
+++ src/components/DataTable/utils.ts
@@ -2,8 +2,8 @@ import { SortDirection } from "./types";
 
 export const sortData = (data: any[], sortDirection: SortDirection, sortColumn: string) => {
     const sortedData = [...data].sort((a, b) => {
-        const firstElem = a[sortColumn] as string;
-        const secondElem = b[sortColumn] as string;
+        const firstElem = a[sortColumn].toString();
+        const secondElem = b[sortColumn].toString();
 
         const compareResult = firstElem.localeCompare(secondElem, undefined, { numeric: true });
         return sortDirection === "asc" ? compareResult : -compareResult;
