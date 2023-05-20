54b07c715ea6d2efaed082220deb9233c63b4494
Helmy Giacoman <helmygb@gmail.com>
2023-05-20 16:53:46 +0200

    Add docstrings to `sortData` function
    To make it easier to understand what this functions does, a short docstrings has been added

--- src/components/DataTable/utils.ts
+++ src/components/DataTable/utils.ts
@@ -1,5 +1,18 @@
 import { SortDirection } from "./types";
 
+/**
+ * Sorts an array of objects based on a specific column in ascending or descending order.
+ *
+ * @param {any[]} data - The array of objects that should be sorted.
+ * @param {string} sortColumn - The key (column) in the objects that should be used for sorting.
+ * @param {SortDirection} sortDirection - The direction of the sorting, either "asc" for ascending or "desc" for descending.
+ *
+ * @returns {any[]} The new sorted array.
+ *
+ * @example
+ * const employees = [{ name: "John", age: 30 }, { name: "Jane", age: 25 }];
+ * sortData(employees, "age", "asc");
+ */
 export const sortData = (data: any[], sortColumn: string, sortDirection: SortDirection) => {
     const sortedData = [...data].sort((a, b) => {
         const firstElem = a[sortColumn].toString();
